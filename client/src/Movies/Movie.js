dbbb7070c1384f51f3bba192132258bb12d65e37
Isaias Garcia-Ariza <isaiasgarciaariza@gmail.com>
2020-03-25 07:48:55 -0700

    updating

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
+  console.log('props',props)
   const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
+    console.log('ID var',id)
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
